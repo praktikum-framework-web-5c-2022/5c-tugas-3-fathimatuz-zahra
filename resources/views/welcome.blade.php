99a34775995327984e9d10e2586669a3e53bfd5e
fzahra21 <2010631170009@student.unsika.ac.id>
2022-10-30 12:30:03 +0700

    Upload Tugas 3

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.master')
+@section('title', 'Welcome')
+@section('content')
+
+    <!--Jumbotron-->
+    <div class="jumbotron text-center">
+        <h1 class="display-5">Selamat Datang di Sistem Informasi Akademik</h1>
+        <p class="lead">Fakultas Ilmu Komputer Universitas Singaperbangsa Karawang</p>
+        <hr class="my-4">
+        <p>- Together Moving Forward -</p>
+      </div>
+
+@endsection
